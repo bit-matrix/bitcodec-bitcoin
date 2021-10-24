fe86b0889d4eb8e1215f167ef4736caf92023f93
Penguen Umut <uuisik@gmail.com>
2021-10-24 21:59:21 +0300

    dist

--- dist/lib/TxCodec.d.ts
+++ dist/lib/TxCodec.d.ts
@@ -2,9 +2,11 @@ import { IBitcodec } from "@bitmatrix/bitcodec";
 import { TxBase, TxSegwit } from "../models/Tx";
 declare class TxCodecClass implements IBitcodec<TxBase | TxSegwit> {
     private length;
-    encodingLength: () => number;
     encodeBytes: number;
     decodeBytes: number;
+    encodingLength: (tx?: TxBase | TxSegwit | undefined) => number;
+    private isSegwit;
+    private isSegwitHex;
     constructor();
     encode: (txObject: TxBase | TxSegwit, buffer?: Buffer | undefined, offset?: number) => Buffer;
     decode: (buffer: Buffer, offset?: number, end?: number | undefined) => TxBase | TxSegwit;
