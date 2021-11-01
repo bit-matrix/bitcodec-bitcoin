cead03744ad2968fa259fc41acd982dae6d5fc44
afarukcali <afarukcali@gmail.com>
2021-11-01 17:18:54 +0300

    block test dummy commit

--- tests/block.test.ts
+++ tests/block.test.ts
@@ -5,11 +5,13 @@ import { datas } from "./data/block";
 datas.forEach((data, index) => {
   test("block decode index:" + index, () => {
     const blockBuffer = BlockCodec.decode(hex2buffer(data.hex));
-    expect(buffer2hex(blockBuffer) as Block).toEqual(data.raw);
+
+    console.log(blockBuffer.txns[0]);
+    // expect(buffer2hex(blockBuffer) as Block).toEqual(data.raw);
   });
 
-  test("block encode index:" + index, () => {
-    const blockHexBuffer = BlockCodec.encode(hex2buffer(data.raw));
-    expect(buffer2hex(blockHexBuffer)).toEqual(data.hex);
-  });
+  // test("block encode index:" + index, () => {
+  //   const blockHexBuffer = BlockCodec.encode(hex2buffer(data.raw));
+  //   expect(buffer2hex(blockHexBuffer)).toEqual(data.hex);
+  // });
 });
